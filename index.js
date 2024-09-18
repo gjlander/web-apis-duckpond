adec9c66f8a10f8c373528d00bfadea8f568b8ba
Garrett Lander <garrett.jlander@gmail.com>
2024-09-18 16:37:11 +0200

    DOM and Web Storage API covered

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-import ducksInThePond from './ducks.js';
+// import ducksInThePond from './ducks.js';
+
+const ducksInThePond = JSON.parse(localStorage.getItem('ducks')) || [];
 
 console.log(ducksInThePond);
+const summonBtn = document.querySelector('#summon');
+const pond = document.querySelector('#pond');
+const form = document.querySelector('#add-form');
+// console.log(summonBtn);
+
+const renderDucks = (ducksArray) => {
+    // console.log('You clicked the summon button!');
+    pond.innerHTML = '';
+    console.log(ducksArray);
+    ducksArray.forEach((duck) => {
+        pond.innerHTML += `<div class="card card-compact bg-base-100 w-96 shadow-xl">
+        <figure>
+        <img
+        src=${duck.imgUrl}
+        alt=${duck.name} />
+        </figure>
+        <div class="card-body">
+        <h2 class="card-title">${duck.name}</h2>
+        <p>${duck.quote}</p>
+        </div>
+        </div>`;
+    });
+};
+summonBtn.addEventListener('click', () => {
+    renderDucks(ducksInThePond);
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name');
+    const imgUrl = document.getElementById('img-url');
+    const quote = document.getElementById('quote');
+
+    const newDuck = {
+        _id: crypto.randomUUID(),
+        name: name.value,
+        imgUrl: imgUrl.value,
+        quote: quote.value,
+    };
+
+    // console.log(newDuck);
+    ducksInThePond.push(newDuck);
+    localStorage.setItem('ducks', JSON.stringify(ducksInThePond));
+
+    renderDucks(ducksInThePond);
+});
